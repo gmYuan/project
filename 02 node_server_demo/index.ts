b53a7a7bde07d00db7f51c9390417cb2b42c4904
Ygm <ygm121351@163.com>
2021-09-01 20:39:31 +0800

    feat: 新增打印语句

--- 02 node_server_demo/index.ts
+++ 02 node_server_demo/index.ts
@@ -2,8 +2,13 @@ import * as http from 'http'
 import {IncomingMessage, ServerResponse} from 'http'
 
 const server = http.createServer()
-server.on('request', (request: IncomingMessage, response) => {
-  console.log('request Method', request.method)
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+  console.log('request Method:', request.method)
+  console.log('request URL:', request.url)
+  console.log('request Header:', request.headers)
+
+
+  // console.log('response:')
   response.end('hi')
 })
 
