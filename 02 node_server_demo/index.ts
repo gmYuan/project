994a2b6b8d5b4233c415d5cf546d902060602800
yuanguangming <yuanguangming@idaddy.cn>
2021-09-01 19:58:22 +0800

    feat: add new

--- 02 node_server_demo/index.ts
+++ 02 node_server_demo/index.ts
@@ -1,8 +1,9 @@
 import * as http from 'http'
+import {IncomingMessage, ServerResponse} from 'http'
 
 const server = http.createServer()
-server.on('request', (request, response) => {
-  console.log('有人请求了')
+server.on('request', (request: IncomingMessage, response) => {
+  console.log('request Method', request.method)
   response.end('hi')
 })
 
