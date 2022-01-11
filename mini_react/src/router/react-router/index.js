8f4fb8970a0d56300c78d8c5738f44a22e071deb
yuanguangming <yuanguangming@idaddy.cn>
2022-01-11 14:16:59 +0800

    feat: 初步实现withRouter

--- mini_react/src/router/react-router/index.js
+++ mini_react/src/router/react-router/index.js
@@ -5,8 +5,7 @@ export {default as __RouterContext} from './RouterContext'
 export {default as matchPath} from './matchPath'
 export {default as Switch} from './Switch';
 export {default as Redirect} from './Redirect';
+export {default as withRouter} from './withRouter';
 
-
-// export {default as withRouter} from './withRouter';
 // export {default as Prompt} from './Prompt';
 // export {useHistory,useLocation,useRouteMatch,useParams} from './hooks';
