550be2c37962dfddb0a684d98f278b035909d8d2
yuanguangming <yuanguangming@idaddy.cn>
2022-02-14 19:08:29 +0800

    feat: 初始化内容

--- mini_react/02_redux/src/store/index.js
+++ mini_react/02_redux/src/store/index.js
@@ -5,8 +5,19 @@ import logger from './logger';
 import thunk from './thunk';
 import promise from './promise';
 let initialState = { counter1: { number:10}, counter2: { number:20} };
-//如果配置了多个中间件，多个中间件会进行级联 promise next=>thunk next=>logger next->store.dispatch
+
+//中间件的核心原因就是重写dispatch方法，在原始的dispatch方法之前和之后加入一些自定义的逻辑
+/* store.dispatch = function(action){
+    console.log('prev state',store.getState());
+    dispatch(action);
+    console.log('next state',store.getState());
+    return action;
+} */
+
+//如果配置了多个中间件，多个中间件会进行级联
+//  promise next=>thunk next=>logger next->store.dispatch
 //let store = applyMiddleware(thunk, promise, logger)(createStore)(combinedReducer,initialState);
+
  let store = createStore(
     combinedReducer,
     initialState,
