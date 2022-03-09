1de99a83f980b11544dfe4872a31ed18e5368fe0
yuanguangming <yuanguangming@idaddy.cn>
2022-03-09 17:02:25 +0800

    feat: 初步开始实现fiber

---
+++ react_fiber/fiber_16/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 03 生成渲染
+let style = { border: '3px solid red', margin: '5px' }
+
+let element3 = (
+  <div id = 'A1' style={style}>
+    A1
+    <div id = 'B1' style={style}>
+      B1
+      <div id = 'C1' style={style}>C1</div>
+      <div id = 'C2' style={style}>C2</div>
+    </div>
+
+    <div id = 'B2' style={style} >
+      B2  
+    </div>
+  </div>
+)
+
+ReactDOM.render(element3, document.getElementById('root'));
