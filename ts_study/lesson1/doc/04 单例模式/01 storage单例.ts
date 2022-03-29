25b43f6a03437af563d84098850d0d8e12b80a4f
Ygm <ygm121351@163.com>
2022-03-29 11:57:44 +0800

    fix: 添加注释

--- ts_study/lesson1/doc/04 单例模式/01 storage单例.ts
+++ ts_study/lesson1/doc/04 单例模式/01 storage单例.ts
@@ -7,20 +7,20 @@
 
 export default class MyLocalStorage {
   count3!: number;
-  // 静态属性和对象属性[实例属性】是类中两大成员
+  // 静态属性 和 对象属性[实例属性】是类中两大成员
   static localstorage: MyLocalStorage//静态引用属性
   static count: number = 3;//静态的基本类型属性
-  constructor() {
+  private constructor() {
     console.log("这是TS的单件设计模式的静态方法的构造器");
   }
 
   // 提供一个外部访问的方法,
   // 通过这个方法用来提供外部得到一个对象的方法
-
   //   1. 带static关键字的方法就是一个静态方法
   //   2. 静态方法和对象无关，外部的对象变量不能调用静态方法和静态属性，
   //   3. 外部可以通过类名来调用
-  //   静态方法不可以访问实例属性或实例方法【对象属性或对象方法】
+
+  //   静态方法 不可以访问 实例属性或实例方法【对象属性或对象方法】
   public static getInstance() {
     // let localstorage = new MyLocalStorage();
     // return localstorage
@@ -38,9 +38,7 @@ export default class MyLocalStorage {
     if (!this.localstorage) {//如果静态对象属性指向创建对象
       console.log("我是一个undefined的静态属性，用来指向一个对象空间的静态属性")
       this.localstorage = new MyLocalStorage()
-
     }
-
     return this.localstorage
   }
 
