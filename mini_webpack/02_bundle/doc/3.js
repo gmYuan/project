d9a931042024064c62b9e3172c465261135dacd3
Ygm <ygm121351@163.com>
2022-04-21 20:27:33 +0800

    feat: add

--- mini_webpack/02_bundle/doc/3.js
+++
@@ -1,7 +0,0 @@
-
-//__webpack_modules__[moduleId].call(
-    module.exports,
-     module, module.exports, __webpack_require__);
-console.log(this===exports,exports===module.exports);
-console.log(exports);
-console.log(module.exports);
