c005802b0e53e46d22b0f122a06cbaae322e5c17
Ygm <ygm121351@163.com>
2022-04-22 16:49:04 +0800

    feat: 异步加载

--- mini_webpack/02_bundle/src/index.js
+++ mini_webpack/02_bundle/src/index.js
@@ -1,2 +1,9 @@
-const title = require('./title')
-console.log('title', title)
+
+
+let lazyLoadButton = document.getElementById('lazyLoad');
+lazyLoadButton.addEventListener('click',()=>{
+    import(/* webpackChunkName: "hello" */'./hello').then(result=>{
+        console.log(result.default);
+    })
+});
+let a =1;
--- mini_webpack/02_bundle/src/title.js
+++ mini_webpack/02_bundle/src/title.js
@@ -1,1 +1,4 @@
-module.exports = 'title'
+module.exports = {
+    name:'title_name',
+    age:'title_age'
+}
