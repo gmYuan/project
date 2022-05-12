d9605b80a962bcb78a79438827261d5828f9edce
Ygm <ygm121351@163.com>
2022-05-12 10:00:10 +0800

    feat: 泛型基础

--- ts_study/lesson2/09 泛型基础.ts
+++ ts_study/lesson2/09 泛型基础.ts
@@ -1,25 +1,33 @@
 export {}
 //创建一个长度为length的数组,里面的值用value填充
-/* function createArray<T>(length: number, value: T): Array<T> {
+
+/* 
+function createArray<T>(length: number, value: T): Array<T> {
   let result: T[] = [];
   for (let i = 0; i < length; i++) {
     result[i] = value;
   }
   return result;
 }
+
 let result = createArray<string>(3,'x');
 console.log(result);
- */
+*/
+
+
 //类数组
-/* function sum(){
+/* 
+function sum(){
     let args:IArguments = arguments;
     for(let a of args){
          console.log(a);
     }
 }
-sum(1,2,3); */
+sum(1,2,3); 
+*/
 
-/* let obj1 = {
+/* 
+let obj1 = {
   [Symbol.iterator]:()=>{
        let i=0;
       function next(){
@@ -32,18 +40,21 @@ sum(1,2,3); */
 };
 for(let key of obj1){
   console.log(key);
-} */
+} 
+*/
 
-/* function * gen(){
+/* 
+function * gen(){
     yield 1;
     yield 2;
 }
 let it = gen();
 let {value,done} = it.next();
 console.log(value,done);
- */
+*/
 
-/*  function sum() {
+/*  
+function sum() {
    let args: IArguments = arguments;
    for (let i = 0; i < args.length; i++) {
      console.log(args[i]);
@@ -51,7 +62,8 @@ console.log(value,done);
  }
  sum(1, 2, 3); */
 
- //泛型类 
+
+ //例1：泛型类 
  class MyArray<T>{
     private list:T[]=[]
     add(value:T){
@@ -66,33 +78,41 @@ let array = new MyArray<number>();
 array.add(1);
 array.add(2);
 array.add(3);
-console.log(array.getMax());
+// console.log(array.getMax());
 
- //与new
- function factory<T>(type:{new():T}):T{
+
+//例2：泛型类  与 new
+function factory<T>( type : { new() : T } ) : T {
   return new type();
  }
  class Person{}
+ 
  let p = factory<Person>(Person);
-console.log(p);
-//泛型接口
-interface Calculate<T>{
-  (a:T,b:T):T
+// console.log('例2', p);
+
+
+//例3：泛型接口
+// 例3.1
+interface Calculate<T> {
+  (a : T,b : T) : T
 }
+
 let sum: Calculate<number> = function (a: number, b: number): number {
   return a + b;
 };
 sum(1,2);
 
-
-interface Calculate2{
+// 例3.2
+interface Calculate2 {
   <T>(a:T,b:T):T
 }
-let sum2: Calculate2 = function <T>(a: T, b: T): T {
+
+let sum2: Calculate2 = function <T> (a: T, b: T): T {
   return a;
 };
 sum2<number>(1, 2);
 
+// 例3.3
 interface Calculate3<T>{
   <U>(a:T,b:T):U
 }
@@ -101,28 +121,27 @@ let sum3: Calculate3<number> = function <U>(a: number, b: number): U {
 };
 sum3<string>(1, 2);
 
-//泛型可以写多个
+
+
+//例4 泛型可以写多个
 function swap<A,B>(tuple:[A,B]):[B,A]{
    return [tuple[1],tuple[0]];
 }
-//默认泛型
-/*  function createArray<T=number>(length: number, value: T): Array<T> {
-   let result: T[] = [];
-   for (let i = 0; i < length; i++) {
-     result[i] = value;
-   }
-   return result;
- } */
- //TODO
-interface T2<T=string>{
 
+
+//例5 默认泛型
+interface T2<T=string>{
 }
 type T22 = T2;
 
-//泛型约束 
-/* function logger<T>(val:T){
+
+//例6 泛型约束 
+/*
+function logger<T>(val:T){
   console.log(val.length);
-} */
+}
+*/
+
 interface LengthWise{
   length:number
 }
@@ -135,7 +154,9 @@ let obj  = {
 }
 type Obj = typeof obj;
 logger2<Obj>(obj);
-//判断兼容不兼容跟extends继承没有一点关系 ,只看形状 有没有对应的属性
+
+
+//例6.2 判断兼容不兼容跟extends继承没有一点关系 ,只看形状 有没有对应的属性
 class GrandFather {
   grandFather:string
 }
@@ -145,11 +166,9 @@ class Father extends GrandFather {
 class Child extends Father{
   child:string
 }
-//约束  
-//或说T能赋值给Father
-//T是Father的子类型
+//extends：约束  
+//或者说T是Father的子类型
 function get<T extends Father>(){
-
 }
 let father = new Father();
 let child = new Child();
