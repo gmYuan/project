c00186f096e0739d5101910fe641bc9d8126f67c
Ygm <ygm121351@163.com>
2022-05-30 17:03:32 +0800

    feat: 简单插件介绍

---
+++ mini_webpack/08_plugin/plugins/DonePlugin.js
@@ -0,0 +1,10 @@
+class DonePlugin {
+  apply(compiler) {
+    debugger;
+    compiler.hooks.done.tapAsync("DonePlugin", (stats, callback) => {
+      console.log("DonePlugin");
+      callback();
+    });
+  }
+}
+module.exports = DonePlugin;
