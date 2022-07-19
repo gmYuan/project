ab739b2476e6f21dfdae2970e6c13d7ab382cb69
Ygm <ygm121351@163.com>
2022-07-19 22:07:57 +0800

    fix: add new

--- mini_vue/01_vue初渲染/src/observer/array.js
+++ mini_vue/01_vue初渲染/src/observer/array.js
@@ -1,32 +1,40 @@
-// 拿到数组原型上的方法 （原来的方法）
+// S1 拿到数组原型上的方法 （原来的方法）
 let oldArrayProtoMethods = Array.prototype;
-// 继承一下   ES5的方法
+
+//S2 创建新的原型基础对象：arrayMethods.__proto__ = Array.prototype
 export let arrayMethods = Object.create(oldArrayProtoMethods);
-let methods = [
-    'push',
-    'pop',
-    'shift',
-    'unshift',
-    'reverse',
-    'sort',
-    'splice'
-]
-methods.forEach(method => {
-    arrayMethods[method] = function(...args) { // this就是observer里的value
-        const result = oldArrayProtoMethods[method].apply(this, args);
-        let inserted;
-        let ob = this.__ob__;
-        switch (method) {
-            case 'push': // arr.push({a:1},{b:2})
-            case 'unshift': //这两个方法都是追加 追加的内容可能是对象类型，应该被再次进行劫持
-                inserted = args;
-                break;
-            case 'splice': // vue.$set原理
-                inserted = args.slice(2); // arr.splice(0,1,{a:1},{a:1},{a:1})
-            default:
-                break;
-        }
-        if(inserted) ob.observeArray(inserted); // 给数组新增的值也要进行观测
-        return result;
+
+// S3 获取 数组中会改变原数组的方法
+let methods = ["push", "shift", "unshift", "pop", "reverse", "sort", "splice"];
+
+// S4 调用的如果是以上七个方法，就会优先读取下面重写的，否则用原来的数组方法
+methods.forEach((method) => {
+  // 接收参数
+  arrayMethods[method] = function (...args) {
+    //S5 要先执行原逻辑
+    const result = oldArrayProtoMethods[method].apply(this, args);
+    let inserted;
+
+    //S6 根据当前数组获取到observer实例
+    let ob = this.__ob__;
+
+    switch (method) {
+      case "push": // arr.push({a:1},{b:2})
+      case "unshift":
+        //这2个方法都是追加 追加的内容可能是对象类型，应该被再次进行劫持
+        inserted = args;
+        break;
+      case "splice": // vue.$set原理
+        inserted = args.slice(2); // arr.splice(0,1,{a:1},{a:1},{a:1})
+      default:
+        break;
     }
+
+    // S7 如果有新增的内容，要进行继续劫持,
+    // 需要观测的是 数组里的每一项，而不是数组
+    if (inserted) ob.observeArray(inserted);
+
+    // S8 返回结果
+    return result;
+  };
 });
--- mini_vue/01_vue初渲染/src/observer/index.js
+++ mini_vue/01_vue初渲染/src/observer/index.js
@@ -1,45 +1,71 @@
 import { isObject } from "../utils";
+import { arrayMethods } from "./array";
+
+// 基本原则：
+// 1.如果数据是对象：会将对象不停的递归 进行劫持
+// 2.如果是数组：会劫持数组的方法，并对数组中不是基本数据类型的 进行劫持
 
 // 劫持对象类型
 export function observe(data) {
-    // 如果是对象才观测
-    if (!isObject(data)) {
-        return;
-    }
-    return new Observer(data)
+  // 如果是对象才观测
+  if (!isObject(data)) {
+    return;
+  }
+  return new Observer(data);
 }
 
-// 对象类型的 劫持中心
+// 劫持中心
 class Observer {
-    //S1 对对象中的所有属性 进行劫持
-    // 缺点：需要递归劫持嵌套对象的属性（增加get/set）,影响性能
-    // 所以 Vue3 改成了 proxy来监听
-    constructor(data) { 
-        this.walk(data);
-    }
+  constructor(data) {
+    // S3 在所有被劫持过的属性上，都增加 __ob__属性
+    // data.__ob__ = this;
+    Object.defineProperty(data, "__ob__", {
+      value: this,
+      enumerable: false, // 不可枚举的
+    });
 
-    walk(data) {
-        Object.keys(data).forEach(key => {
-            defineReactive(data, key, data[key]);
-        })
-    }
+    if (Array.isArray(data)) {
+      //S2 单独处理 数组劫持的逻辑，因为数组并不能只是 简单检测它的key变化
 
+      //S2.1 对数组原来的方法进行改写：切片编程/高阶函数/代理模式
+      data.__proto__ = arrayMethods;
+
+      //S2.2 如果数组中的数据是对象类型，需要监控对象的变化
+      this.observeArray(data);
+    } else {
+      //S1 对对象中的所有属性 进行劫持
+      // 缺点：需要递归劫持嵌套对象的属性（增加get/set）,影响性能
+      // 所以 Vue3 改成了 proxy来监听
+      this.walk(data);
+    }
+  }
+
+  observeArray(data) {
+    // 对数组成员里的 嵌套数组/对象类型，进行递归劫持
+    data.forEach((item) => observe(item));
+  }
+
+  walk(data) {
+    Object.keys(data).forEach((key) => {
+      defineReactive(data, key, data[key]);
+    });
+  }
 }
 
 // vue2会对对象进行遍历 ==> 每个属性用defineProperty重新定义
-function defineReactive(data,key,value){ 
-    observe(value); // 处理对象嵌套 ==> 递归调用
+function defineReactive(data, key, value) {
+  observe(value); // 处理对象嵌套 ==> 递归调用
 
-    Object.defineProperty(data, key, {
-        get(){
-            return value
-        },
-        set(newV){ 
-            // Todo 更新视图
-            console.log('监测到值发生了变化')
-            //如果赋值的是一个新对象 ，也需要对这个新对象 进行劫持
-            observe(newV);
-            value = newV;
-        }
-    })
+  Object.defineProperty(data, key, {
+    get() {
+      return value;
+    },
+    set(newV) {
+      // Todo 更新视图
+      console.log("监测到值发生了变化");
+      //如果赋值的是一个新对象 ，也需要对这个新对象 进行劫持
+      observe(newV);
+      value = newV;
+    },
+  });
 }
