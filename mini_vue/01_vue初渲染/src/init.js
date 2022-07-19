4258f4114f5ebbee34a08a6d5262a6ce899598ca
Ygm <ygm121351@163.com>
2022-07-20 00:11:54 +0800

    feat: 初步整理parseHTML，存在问题

--- mini_vue/01_vue初渲染/src/init.js
+++ mini_vue/01_vue初渲染/src/init.js
@@ -1,41 +1,40 @@
 import { initState } from "./state";
-import { compileToFunctions } from './compiler'
+import { compileToFunctions } from "./compiler/index";
 
-export function initMixin(Vue){
-    Vue.prototype._init = function (options) {
-        const vm = this;
-        vm.$options = options;
+export function initMixin(Vue) {
+  Vue.prototype._init = function (options) {
+    const vm = this;
+    vm.$options = options;
 
-        // S2 对初始化数据的类型进行 逻辑细分 ==> Props/ Data/ Computed等
-        initState(vm);
+    // S2 对初始化数据的类型进行 逻辑细分 ==> Props/ Data/ Computed等
+    initState(vm);
 
-        // S3 准备模板编译
-        if(vm.$options.el){
-            vm.$mount(vm.$options.el);
-        }
-      
+    // S3 准备模板编译
+    if (vm.$options.el) {
+      vm.$mount(vm.$options.el);
     }
+  };
 
-    // S3 编译过程
-    // 把模板转化成 对应的渲染函数render ==>
-    // 虚拟dom vnode: 增加额外的对象属性 && 用diff算法来 更新虚拟dom ==>
-    // 生成真实dom
-    Vue.prototype.$mount = function (el) {
-        const vm = this;
-        const options = vm.$options
-        el = document.querySelector(el);
-        vm.$el = el;
+  // S3 编译过程
+  // 把模板转化成 对应的渲染函数render ==>
+  // 虚拟dom vnode: 增加额外的对象属性 && 用diff算法来 更新虚拟dom ==>
+  // 生成真实dom
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    const options = vm.$options;
+    el = document.querySelector(el);
+    vm.$el = el;
 
-        // 编译优先级： render方法 > template属性 > el的内容
-        if(!options.render){
-            let template = options.template;
-            if(!template && el){ // 也没有传递template 就取el的内容作为模板
-                template = el.outerHTML;
-            }
-            //S3.1 将模板编译成render函数
-            const render = compileToFunctions(template);
-            options.render = render
-        }
+    // 编译优先级： render方法 > template属性 > el的内容
+    if (!options.render) {
+      let template = options.template;
+      if (!template && el) {
+        // 也没有传递template 就取el的内容作为模板
+        template = el.outerHTML;
+      }
+      //S3.1 将模板编译成render函数
+      const render = compileToFunctions(template);
+      options.render = render;
     }
-
+  };
 }
