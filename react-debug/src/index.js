d582f9b4617101c392a3fcad55eff5d4e66b1553
yuanguangming <yuanguangming@idaddy.cn>
2022-07-22 10:26:49 +0800

    feat: 初始化调试

---
+++ react-debug/src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import App from './App'
+import './index.css';
+
+const root = document.getElementById('root')
+
+// Concurrent mode
+ReactDOM.createRoot(root).render(<App />);
+
+// blocking mode
+// ReactDOM.createBlockingRoot(root).render(<App />);
+
+// Sync mode
+// ReactDOM.render(<App />, root);
+
+// console.log('React 源码调试，当前版本：' + React.version);
