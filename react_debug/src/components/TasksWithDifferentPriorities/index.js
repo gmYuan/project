734258dc89f1920c2daaacbc27b4e2cdfe52e85c
yuanguangming <yuanguangming@idaddy.cn>
2022-08-18 14:14:08 +0800

    fix: 调试前准备

--- react_debug/src/components/TasksWithDifferentPriorities/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './indes.css'
-import NewDemo from './NewDemo'
-import OriginDemo from './OriginDemo'
-
-export default OriginDemo
-// export default NewDemo
--- react_debug/src/index.js
+++ react_debug/src/index.js
@@ -1,17 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App'
-
 const root = document.getElementById('root')
 
 // Concurrent mode
 ReactDOM.createRoot(root).render(<App />);
-
-
-
-// blocking mode
-// ReactDOM.createBlockingRoot(root).render(<App />);
-
-// Sync mode
-// ReactDOM.render(<App />, root);
-
-// console.log('React 源码调试，当前版本：' + React.version);
