951e9deea147a8bea7d93b70084dc366657bab2a
Ygm <ygm121351@163.com>
2023-03-13 22:45:11 +0800

    feat: 更新debug

--- vue-2.6.14-debug/debug/01_响应式原理/index.js
+++ vue-2.6.14-debug/debug/01_响应式原理/index.js
@@ -4,6 +4,31 @@ import "../../dist/vue.js";
 const app = new Vue({
   el: "#app",
   data: {
-    message: "hello Vue2222",
+    name: "haha",
+    info: {
+      mid: 123,
+    },
+    friends: ["朋友a", "朋友b"],
+  },
+  computed: {},
+
+  watch: {},
+
+  methods: {
+    handleChage1() {
+      console.log("修改了mid");
+      this.info.mid = "456";
+      // this.info = { name: "新name" };
+      this.info.money = 200;
+    },
+    handleChage2() {
+      console.log("修改了money");
+      this.info.money = 100;
+    },
+    handleChage3() {
+      console.log("修改了friends");
+      this.friends[1] = "新朋友啊";
+      // this.$set(this.friends, "1", "新朋友");
+    },
   },
 });
